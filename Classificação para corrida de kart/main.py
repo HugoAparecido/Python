f6bd4421f3c4566af36c93e3737f75d3cb35404e
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-10-10 18:32:37 -0300

    10/10/2023

---
+++ Classificação para corrida de kart/main.py
@@ -0,0 +1,23 @@
+qtd_pilotos = int(input())
+qtd_voltas = int(input())
+qtd_voltas_invalidas = int(input())
+participantes = []
+voltas = []
+nomes = 1
+while nomes <= qtd_pilotos:
+    participantes.append(input())
+    nomes += 1
+volta = 1
+nomes = 1
+while volta <= qtd_voltas:
+    voltas.append(input())
+    while nomes <= qtd_pilotos:
+        if voltas[volta[0, 1, 3]] == participantes[volta[0]] + participantes.[volta[1,2]].is
+        nomes += 1
+    volta += 1
+voltas.pop(qtd_voltas_invalidas)
+
+saida = 1
+while saida <= qtd_pilotos:
+    print(str(saida) + ' ' + participantes[saida-1])
+    saida += 1
