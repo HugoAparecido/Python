fc12a0059042106f1800724c0a3d9bd7adf4b119
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-10-11 14:06:13 -0300

    Update main.py

--- Classificação para corrida de kart/main.py
+++ Classificação para corrida de kart/main.py
@@ -1,7 +1,13 @@
+def minimo(lista):
+    minimo_atual = lista[0]  # Começa com o primeiro elemento da lista
+    for item in lista[1:]:  # Percorre o restante da lista
+        if item < minimo_atual:
+            minimo_atual = item  # Encontra um novo mínimo
+    return minimo_atual
 qtd_pilotos = int(input())
 qtd_voltas = int(input())
 qtd_voltas_invalidas = int(input())
-participantes = []
+participantes = [[], []]
 voltas = []
 nomes = 1
 while nomes <= qtd_pilotos:
@@ -10,13 +16,19 @@ while nomes <= qtd_pilotos:
 volta = 1
 nomes = 1
 while volta <= qtd_voltas:
-    voltas.append(input())
+    voltas.append(str(input()))
     while nomes <= qtd_pilotos:
-        if voltas[volta[0, 1, 3]] == participantes[volta[0]] + participantes.[volta[1,2]].is
+        if str(voltas[volta-1][0] + voltas[volta-1][1] + voltas[volta-1][2]) == str(participantes[volta-1][0] +
+                                                                                    participantes[volta-1][1].lower() +
+                                                                                    participantes[volta-1][2].lower()):
+            participantes[volta-1].append(" " + voltas[volta-1])
         nomes += 1
     volta += 1
 voltas.pop(qtd_voltas_invalidas)
-
+nomes = 1
+primeiro_lugar = participantes[0]
+while nomes <= qtd_pilotos:
+    nomes += 1
 saida = 1
 while saida <= qtd_pilotos:
     print(str(saida) + ' ' + participantes[saida-1])
