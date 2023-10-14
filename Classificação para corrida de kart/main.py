6dce7193fb7d8a004666f926e49fc82df8edb78c
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-10-14 16:58:11 -0300

    Update main.py

--- Classificação para corrida de kart/main.py
+++ Classificação para corrida de kart/main.py
@@ -1,39 +1,58 @@
-# entrada do usuário
+# entrada do usuário para os números
 entrada = input()
-# dividindo a entrada em uma lista de strings
+# dividindo a entrada em uma lista de strings (a cada espaço terá uma nova posição
 numeros_str = entrada.split()
 # convertendo a lista de stings para uma lista de inteiros
 numeros = [int(num) for num in numeros_str]
+# posição 0 corresponde ao número de pilotos
 qtd_pilotos = numeros[0]
+# posição 1 corresponde ao número de voltas
 qtd_voltas = numeros[1]
+# posição 2 corresponde ao número de voltas inválidas
 qtd_voltas_invalidas = numeros[2]
+# declaração de listas (vetores)
 participantes = []
 voltas = []
-nomes = 1
-while nomes <= qtd_pilotos:
+melhor_volta = []
+# Pegando os nomes dos corredores e colocando-os no vetor participantes
+controle = 0
+while controle < qtd_pilotos:
     participantes.append(input())
-    nomes += 1
-volta = 0
-while volta < qtd_voltas:
-    voltas.append(str(input()))
-    volta += 1
+    controle += 1
+# Pegando as informações das voltas e colocando-as no vetor voltas
+controle = 0
+while controle < qtd_voltas:
+    voltas.append(input())
+    controle += 1
 voltas.pop(qtd_voltas_invalidas)
-nomes = 0
-volta = 0
-while volta < qtd_voltas - qtd_voltas_invalidas:
-    while nomes < len(participantes):
-        print(participantes[nomes])
-        '''if participantes[nomes][-4] != '.' and participantes[nomes][0] == voltas[volta][0]:'''
-        participantes[nomes] += voltas[volta][3:]
-        print(participantes[nomes])
-        nomes += 1
-    print(voltas[volta])
-    volta += 1
-nomes = 1
-primeiro_lugar = participantes[0]
-while nomes <= qtd_pilotos:
-    nomes += 1
-saida = 1
-while saida <= qtd_pilotos:
-    print(str(saida) + ' ' + participantes[saida - 1])
-    saida += 1
+controle = 0
+while controle < len(voltas):
+    if controle < 4:
+        melhor_volta.append(voltas[controle])
+    else:
+        controle2 = 0
+        while controle2 < len(melhor_volta):
+            if voltas[controle][0:3] == melhor_volta[controle2][0:3]:
+                if int(voltas[controle][4]) < int(melhor_volta[controle2][4]):
+                    del melhor_volta[controle2]
+                    melhor_volta.append(voltas[controle])
+                elif voltas[controle][4] == melhor_volta[controle2][4] and not float(
+                        voltas[controle][6:]) >= float(melhor_volta[controle2][6:]):
+                    del melhor_volta[controle2]
+                    melhor_volta.append(voltas[controle])
+            controle2 += 1
+    controle += 1
+controle = 0
+while controle < len(participantes):
+    controle2 = 0
+    while controle2 < len(melhor_volta):
+        if participantes[controle][0] + participantes[controle][1:3].upper() == melhor_volta[controle2][0:3]:
+            participantes[controle] += " " + melhor_volta[controle2][4:]
+        controle2 += 1
+    controle += 1
+controle = 1
+# Reorganizando a lista em ordem crescente numérica
+participantes = sorted(participantes, key=lambda x: int(''.join(filter(str.isdigit, x))))
+while controle <= qtd_pilotos:
+    print(str(controle) + ' ' + participantes[controle - 1])
+    controle += 1
