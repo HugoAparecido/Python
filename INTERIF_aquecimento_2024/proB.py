c1a437f87324e5363fb3ed5d0c1c763086850a8f
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-07-30 18:45:07 -0300

    Update proB.py

--- INTERIF_aquecimento_2024/proB.py
+++ INTERIF_aquecimento_2024/proB.py
@@ -1,11 +1,9 @@
 palavra = list(input())
 elementos = list(set(palavra))
 restoQtd = []
-print(elementos)
 for elemento in elementos:
     restoQtd.append(palavra.count(elemento) % 2)
-print(restoQtd)
 if(restoQtd.count(1) == 1):
-    print(1)
+    print("1")
 else:
-    print(0)
+    print("0")
