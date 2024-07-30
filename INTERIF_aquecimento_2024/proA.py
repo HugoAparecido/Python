92bb2cc2028a83cbb8dd78097b0f24fe804c19b7
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-07-30 16:25:30 -0300

    Update proA.py

--- INTERIF_aquecimento_2024/proA.py
+++ INTERIF_aquecimento_2024/proA.py
@@ -1,7 +1,24 @@
 entrada = input().split()
 qtdCidades = int(entrada[0])
 qtdCaminhos = int(entrada[1])
-while(entrada[1]):
+caminhos = []
+destino = input().split()
+passou = []
+while(qtdCaminhos):
     entradaAtual = input().split()
-
-    entrada[1]-=1
+    caminhos.append(entradaAtual)
+    qtdCaminhos -= 1
+destinoAtual = [destino[1]]
+qtdKilometros = 0
+i = 0
+while(True):
+    for caminho in caminhos:
+        if(caminho[0] == destino[0] and caminho[1] == destino[1]):
+            qtdKilometros += int(caminho[2])
+            break
+        elif(caminho[1] == destinoAtual):
+            passou.append(caminho)
+            destinoAtual.append(caminho[0])
+    i+=1
+print(passou)
+print(f"Percurso:")
