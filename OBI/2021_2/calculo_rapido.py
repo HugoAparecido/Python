9b26a4099591d61eed054bdd6a3724161089e087
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-08-13 19:35:41 -0300

    Create calculo_rapido.py

---
+++ OBI/2021_2/calculo_rapido.py
@@ -0,0 +1,11 @@
+S = int(input())
+A = int(input())
+B = int(input())
+qtd_somas = 0
+while(A<=B):
+    numero_str = str(A)
+    numeros = [int(digito) for digito in numero_str]
+    if(sum(numeros) == S):
+        qtd_somas += 1
+    A+=1
+print(qtd_somas)
