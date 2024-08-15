ea6a85377dc49d476a7a5b83e76326832b7586c0
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-08-15 20:03:30 -0300

    Update media_mediana.py

--- OBI/2021_2/media_mediana.py
+++ OBI/2021_2/media_mediana.py
@@ -5,6 +5,12 @@ B = int(B)
 C = 0
 mediana = sorted([A, B, C])
 while ((A+B+C)/3) != mediana[1]:
-    C += 1
+    if (C >= 0):
+        C += 1
+    else:
+        C -= 1
+    if(C == 10**9):
+        print("Chegou")
+        C = -1
     mediana = sorted([A, B, C])
 print(C)
