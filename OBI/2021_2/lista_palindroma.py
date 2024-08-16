37a2fea2f0315f07827b55d200c0ea8a7ed18747
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-08-15 21:08:55 -0300

    Update lista_palindroma.py

--- OBI/2021_2/lista_palindroma.py
+++ OBI/2021_2/lista_palindroma.py
@@ -1,6 +1,28 @@
+def verificar_simetria(array):
+    n = len(array)
+    for i in range(n // 2):
+        if array[i] != array[n - i - 1]:
+            return False
+    return True
+def substituir_valores(array, comeco):
+    if comeco != len(array) - 1:
+        array[comeco] = array[comeco] + array[comeco + 1]
+        del array[comeco+1]
 N = input()
-Numeros = input().split()
+linha = input().split()
+Numeros = [int(num) for num in linha]
+Mudancas = [0]
 for i in range(len(Numeros)):
-
-    if (Numeros[i] == Numeros[N - i + 1]):
-        break
+    print(i)
+    Mudancas.append(0)
+    j = i
+    while (not verificar_simetria(Numeros)) or len(Numeros) > j:
+        Mudancas[i+1] += 1
+        substituir_valores(Numeros, j)
+        print(Numeros)
+        print(Mudancas)
+        j += 1
+        print(j)
+    Numeros = [int(num) for num in linha]
+Mudancas = sorted(Mudancas)
+print(Mudancas[0])
