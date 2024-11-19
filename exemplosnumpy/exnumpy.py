a0f584028bccf5544ba832d9ef5da7fcd1af5974
Hugo Aparecido Gregorio de Almeida <120059026+HugoAparecido@users.noreply.github.com>
2024-11-19 20:09:17 -0300

    Terminando exemplos com numpy

--- exemplosnumpy/exnumpy.py
+++ exemplosnumpy/exnumpy.py
@@ -1,8 +1,11 @@
+import numpy.matlib
 import numpy as np
 
 # Criar e inicializar as matrizes A e B
 A = np.array([[1, 14, 6], [4, 5, 9], [13, 11, 2]])
+
 B = np.array([[7, 15, 18], [9, 10, 12], [8, 16, 9]])
+
 # Mostrar as matrizes criadas
 print(f"Matriz A:\n{A}")
 print(f"Matriz A:\n{B}")
@@ -40,3 +43,63 @@ print(A.T)
 print("\n Determinante da matriz B:")
 det = np.linalg.det(B)
 print(det)
+
+# Gerar matrizes especiais
+
+# Gerar matriz vazia
+V = np.matlib.empty((3, 4))
+print("Matriz Gerada:\n", V)
+
+# Gerar matriz preenchida com zeros
+Z = np.matlib.zeros((3, 4))
+print(f'\nMatriz 3x4 de zeros:\n {Z}')
+
+# Gerar matriz preenchida com uns
+U = np.matlib.ones((3, 4))
+print(f'\nMatriz 3x4 de uns:\n {U}')
+
+# Gerar matriz preenchida com uns
+U2 = np.matlib.ones(shape=10, dtype=int)
+print(f'\nArray de uma dimensão de uns:\n {U2}')
+
+# Gerar matriz identidade 5x5
+I = np.matlib.eye(5, k=0)
+print(f'Matriz identidade 5x5:\n{I}')
+
+I2 = np.matlib.identity(5)
+print(f'Matriz identidade 5x5:\n{I2}')
+
+# Remodelar um array 1D em array 2D
+
+vetor = np.array([2, 4, 6, 1, 3, 5])
+C = np.reshape(vetor, (2, 3))
+print(f'Array original:\n{vetor}')
+print(f'Array remodelado 2D:\n{C}')
+
+# Concatenação de matrizes
+
+# Concatenação na vertical
+vertical = np.vstack((A, B))
+print(f'Matrizes concatenadas na vertical:\n{vertical}')
+
+# Concatenação na horizontal
+horizontal = np.hstack((A, B))
+print(f'Matrizes concatenadas na horizontal:\n{horizontal}')
+
+# Funções estatísticas em matrizes
+
+print(f'Média: {round(np.mean(A), 2)}')
+print(f'Mediana: {np.median(A)}')
+print(f'Mediana de cada coluna: {np.median(A, axis=0)}')
+print(f'Valor mínimo: {np.min(A)}')
+print(f'Valor máximo: {np.max(A)}')
+print(f'Desvio padrão: {np.std(A)}')
+print(f'25º percentil: {np.percentile(A, 25)}')
+
+# Salvar a matriz em um arquivo de texto
+# np.savetxt('dados.txt', B)
+
+# Carregar os dados a partir de um arquivo de texto
+dados = np.loadtxt('dados.txt')
+
+print(dados)
